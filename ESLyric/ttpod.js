49bd1b09a537cd5cdfa6aa989f9a77acd3dd6312
Je <jeannela@foxmail.com>
2016-07-24 13:32:36 +0800

    ttpod.js: update 2016/7/24
    using new Interfaces provied by new version of ESLyric.

--- ESLyric/ttpod.js
+++ ESLyric/ttpod.js
@@ -1,21 +1,22 @@
 ﻿// ======================================================
 // Created by Jeannela <jeannela@foxmail.com>
-// Updated: 2015-12-21 17:41
+// Updated: 2016-07-24 10:15
 // Refer: www.bkjia.com/jQuery/1017034.html
 // ======================================================
 
 /** 说明：
- * ESLyric 的天天动听歌词搜索脚本，仅适用于 ESLyric！*/
+ * - ESLyric 的天天动听歌词搜索脚本，仅适用于 ESLyric！
+ * - 只适用于 ESLyric version 0.3.5+, 老版本的 ESLyric 还请用老脚本
+ *   (当然还是推荐升级 ESLyric 到最新版) */
 
-var xmlHttp = new ActiveXObject("Msxml2.ServerXMLHTTP.6.0");
-var debug = false; // 如果要调试的话，改为 true.
+var debug = true; // 如果要调试的话，改为 true.
 
 function get_my_name() {
-	return "天天动听";
+	return "TTPod|天天动听";
 }
 
 function get_version() {
-	return "0.1.3";
+	return "0.2.0";
 }
 
 function get_author() {
@@ -24,92 +25,70 @@ function get_author() {
 
 function start_search(info, callback) {
 
-	var searchURL, lyricURL;
+    var url;
 	var title = info.Title;
 	var artist = info.Artist;
 
-	// Process keywords
-	title = process_keywords(title);
-	artist = process_keywords(artist);
-	searchURL = "http://so.ard.iyyin.com/s/song_with_out?q=" + encodeURIComponent(title) + "+" + encodeURIComponent(artist);
-	debug && console(searchURL);
+    // New method instead of xmlhttp...
+    var http_client = utils.CreateHttpClient();
 
-	try {
-		xmlHttp.open("GET", searchURL, false);
-		xmlHttp.send();
-	} catch(e) {
-		console("Search failed");
-		return;
-	}
+    url = generate_url(title, artist, true, -1);
+	//debug && console(url);
+    var json_txt = http_client.Request(url);
+    if (http_client.StatusCode != 200) {
+        console("Request url[" + url + "] error : " + http_client.StatusCode);
+        return;
+    }
 
-	var newLyric = fb.CreateLyric();
+    var _new_lyric = callback.CreateLyric();
 
-	if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+    // parse json_txt
+    //debug && console(json_txt);
+    var data = json(json_txt)["data"];
+    debug && console("data.length == " + data.length);
+    // download lyric
+    for (var j = 0; j < data.length; j++) {
+        if (callback.IsAborting()) {
+            console("user aborted");
+            break;
+        }
+        url = generate_url(data[j].singer_name, data[j].song_name, false, data[j].song_id);
+        var json_txt = http_client.Request(url);
+        if (http_client.StatusCode != 200) {
+            console("Request url[" + url + "] error : " + http_client.StatusCode);
+            continue;
+        }
+        // add to eslyric
+        try {
+            var _lrc_txt = json(json_txt).data.lrc;
+            if (_lrc_txt.indexOf("无歌词") > -1) {
+                continue;
+            }
+            _new_lyric.LyricText = _lrc_txt;
+            _new_lyric.Title = data[j].song_name;
+            _new_lyric.Artist = data[j].singer_name;
+            _new_lyric.Album = data[j].album_name;
+            _new_lyric.Location = url,
+            callback.AddLyric(_new_lyric);
+            (j % 2 == 0) && callback.Refresh();
+        } catch (e) {
+            continue;
+        }
+    }
+    _new_lyric.Dispose();
 
-		var results = json(xmlHttp.responseText);
-		var data = results.data;
-
-		var tracks = [];
-
-		debug && console("data.length > " + data.length);
-		// Download lyric
-		for (var j = 0; j < data.length; j++) {
-			try {
-				xmlHttp.open("GET", generate_url(data[j].singer_name, data[j].song_name, data[j].song_id), false);
-				xmlHttp.send();
-			} catch(e) { continue; }
-
-			if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-				try {
-					var lrc_ = json(xmlHttp.responseText).data.lrc;
-					if (lrc_ && lrc_ != "") { 
-						tracks.push({
-							lyric: lrc_,
-							title: data[j].song_name,
-							artist: data[j].singer_name
-						});
-
-						debug && console(lrc_);
-					};
-				} catch(e) {
-					continue;
-				}
-			}
-		}
-
-		if (tracks.length > 0) {
-			// 排除相同的歌词（完全相同的）
-			for (var i = 0; i < tracks.length; i++) {
-				for (var j = i+1; j < tracks.length; j++) {
-					if (tracks[i].lyric === tracks[j].lyric) {
-						tracks.splice(j, 1);
-						j--;
-					}
-				}
-			}
-
-			// 排除无效歌词
-			for (var i = 0; i < tracks.length; i++) {
-				if (tracks[i].lyric.indexOf("无歌词") > -1) {
-					tracks.splice(i, 1);
-				}
-			};
-
-			for (var i = 0; i < tracks.length; i++) {
-				newLyric.LyricText = tracks[i].lyric;
-				newLyric.Title = tracks[i].title;
-				newLyric.Artist = tracks[i].artist;
-				newLyric.Source = get_my_name();
-				callback.AddLyric(newLyric);
-			}
-		}
-
-	}
-	newLyric.Dispose();
 }
 
-function generate_url(artist, title, song_id) {
-	return "http://lp.music.ttpod.com/lrc/down?lrcid=&artist=" + encodeURIComponent(artist) + "&title=" + encodeURIComponent(title) + "&song_id=" + song_id;
+function generate_url(artist, title, query, song_id) {
+    var url = "";
+    if (query) {
+        title = process_keywords(title);
+        artist = process_keywords(artist);
+        url = "http://so.ard.iyyin.com/s/song_with_out?q=" + encodeURIComponent(title) + "+" + encodeURIComponent(artist);
+    } else {
+        url = "http://lp.music.ttpod.com/lrc/down?lrcid=&artist=" + encodeURIComponent(artist) + "&title=" + encodeURIComponent(title) + "&song_id=" + song_id;
+    }
+    return url;
 }
 
 function process_keywords(str) {
@@ -134,7 +113,7 @@ function json(text)
 }
 
 function console(s) {
-	fb.trace("TTpod: " + s);
+	fb.trace("TTPod: " + s);
 };
 
 
